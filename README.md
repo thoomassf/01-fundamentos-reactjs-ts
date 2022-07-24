8260189ab2867cee4a40ecf9110df03a8cef16f8
Thomas Farias <thomasfarias19@gmail.com>
2022-07-24 13:31:25 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 01-fundamentos-reactjs-ts
+ Projeto que engloba os fundamentos do React JS com typescript
---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post"
+
+import './global.css';
+import styles from './App.module.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/thoomassf.png',
+      name: 'Thomas Farias',
+      role: 'Web Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, eveto da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content:'jane.design/doctorcare' },
+    ],
+    publisheAt: new Date('2022-07-03 19:03:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/thoomassf.png',
+      name: 'Thomas Farias',
+      role: 'Web Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, eveto da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content:'jane.design/doctorcare' },
+    ],
+    publisheAt: new Date('2022-06-30 18:10:00')
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return(
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publisheAt={post.publisheAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
